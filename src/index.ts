2e9b8831811890ef31efd67a9c2fde0475f42b2e
Nick R <mcwin10getrekt@gmail.com>
2021-12-19 16:32:19 +0530

    feat: error handling

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ import { Dobro } from './lib/structures/Client';
 export const client = new Dobro();
 
 client.init();
+
+process.on('unhandledRejection', (reason, p) => {
+	client.logger.error(reason);
+});
